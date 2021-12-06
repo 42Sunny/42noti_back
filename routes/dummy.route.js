f417466c27a6d3638201089a7b1be6631d26dc81
srngch <srngch.dev@gmail.com>
2021-12-06 21:09:25 +0900

    DUMMY: add api get a dummy event

--- routes/dummy.route.js
+++ routes/dummy.route.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const data = require('../data/dummy');
+const dummyData = require('../data/dummy');
 
 const router = express.Router();
 
+router.use('/event/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const event = dummyData.data.find(event => event.id === id);
+  console.dir(event);
+  res.json(event);
+});
+
 router.use('/', (req, res) => {
-  res.json(data);
+  res.json(dummyData);
 });
 
 module.exports = router;
