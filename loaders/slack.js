529a0f2836d6c9427541815df3ff010a0b2eb2b1
srngch <srngch.dev@gmail.com>
2021-12-06 20:01:36 +0900

    slack test messages

--- loaders/slack.js
+++ loaders/slack.js
@@ -12,6 +12,10 @@ const test = async () => {
   const sarchoiUserId = await findUserIdByUsername('sarchoi');
   const sarchoiDmChannelId = await findDmChannelId(sarchoiUserId);
   await sendMessage(sarchoiDmChannelId, 'Hello, DM!');
+
+  const yunjungUserId = await findUserIdByUsername('yunjung');
+  const yunjungDmChannelId = await findDmChannelId(yunjungUserId);
+  await sendMessage(yunjungDmChannelId, 'Hello, DM!');
 };
 
 module.exports = async () => {
