22ba7ff95c761ab85d6d4d21fba93e780f84ca6e
sarchoi@42 <srngch.dev@gmail.com>
2021-12-12 14:45:17 +0900

    Update passport.js

--- loaders/passport.js
+++ loaders/passport.js
@@ -9,7 +9,7 @@ module.exports = app => {
       {
         clientID: env.fortytwoClientId,
         clientSecret: env.fortytwoClientSecret,
-        callbackURL: `https://test-42meetup.herokuapp.com/login/42/return`,
+        callbackURL: `http://test-42meetup.herokuapp.com/login/42/return`,
         passReqToCallback: true,
       },
       async (req, accessToken, refreshToken, profile, done) => {
