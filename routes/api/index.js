72cd1f1ec1f5a6d6077abb5af2725616a5d27a43
srngch <srngch.dev@gmail.com>
2021-12-21 02:25:40 +0900

    modify: api route

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const {
   apiCampusController,
   apiUserController,
-  apiCampusEventsController,
+  apiSeoulCampusEventsController,
+  apiEventController,
   apiUserEventsController,
 } = require('../../controllers/api');
+const { get42Api } = require('../../utils/42api');
 
 const router = express.Router();
 
@@ -22,9 +24,32 @@ router
     res.status(200).end();
   });
 
-router.use('/campus/events', apiCampusEventsController);
+router.use('/events', apiSeoulCampusEventsController);
+router.use('/event/:eventId', apiEventController);
 router.use('/campus', apiCampusController);
 router.use('/user/:intraLoginId/events', apiUserEventsController);
+router.use('/roles', async (req, res) => {
+  const path = `/v2/roles`;
+  try {
+    const data = await get42Api(path);
+    if (data) {
+      res.json(data);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+router.use('/user/:intraLoginId/roles', async (req, res) => {
+  const path = `/v2/users/${req.params.intraLoginId}/roles`;
+  try {
+    const data = await get42Api(path);
+    if (data) {
+      res.json(data);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
 router.use('/user/:intraLoginId', apiUserController);
 
 module.exports = router;
